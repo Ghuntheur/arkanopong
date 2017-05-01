ee029962ef419dc947f15dcaf649b18c12706cbc
Florian <zobeleflorian@gmail.com>
2017-05-01 13:41:21 +0200

    ajout des fichiers de geometry de base (Point2D)

---
+++ include/geometry.h
@@ -0,0 +1,11 @@
+#ifndef GEOMETRY_H_
+#define GEOMETRY_H_
+
+typedef struct Point{
+	float x;
+	float y;
+}Point;
+
+Point PointXY(float x, float y);
+
+#endif
---
+++ src/geometry.c
@@ -0,0 +1,10 @@
+#include "geometry.h"
+
+Point PointXY(float x, float y){
+	Point p;
+
+	p.x = x;
+	p.y = y;
+	
+	return p;
+}
